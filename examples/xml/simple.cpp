da8b1a5e9a08016177e3b4fdc1f990b3c5752751
Spencer E. Olson <olsonse@umich.edu>
2010-12-16 15:11:09 -0700

    version from git-describe; cmake usability improvements

--- examples/xml/simple.cpp
+++ examples/xml/simple.cpp
@@ -4,10 +4,16 @@
 
 
 #ifndef   XML_FILENAME
-#  define XML_FILENAME  "file.xml"
+#  define XML_FILENAME  file.xml
 #endif
 
+#ifndef   XML_FILENAME
+#  define XML_FILENAME  file-2.xml
+#endif
+
+#include <xylose/XSTR.h>
 #include <xylose/xml/Doc.h>
+
 #include <iostream>
 #include <cstdlib>
 
@@ -17,10 +23,10 @@ namespace xml = xylose::xml;
 int main(int argc, char **argv) {
   const char * xml_filename = getenv("XML_FILENAME");
   if ( !xml_filename )
-    xml_filename = XML_FILENAME;
+    xml_filename = XSTR(XML_FILENAME);
 
   xml::Doc doc(xml_filename);
-  xml::Doc doc2(XML_FILENAME2);
+  xml::Doc doc2(XSTR(XML_FILENAME2));
 
   xml::Context stuff = doc.find("/Stuff");
   stuff.extend( doc2.find("/other") );
